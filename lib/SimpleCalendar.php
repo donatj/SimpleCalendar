dc53ea92dba1001d52df8ec3ef8cce45e2e31d62
Jesse Donat <j.donat@capstonedigital.com>
2013-03-07 17:36:07 -0600

    Executed code formatter on the library

--- lib/SimpleCalendar.php
+++ lib/SimpleCalendar.php
@@ -1,77 +1,77 @@
 <?php
 
 /**
-* Simple Calendar
-* 
-* @author Jesse G. Donat
-* @link http://donatstudios.com
-* @license http://opensource.org/licenses/mit-license.php
-* 
-*/
+ * Simple Calendar
+ *
+ * @author Jesse G. Donat
+ * @link http://donatstudios.com
+ * @license http://opensource.org/licenses/mit-license.php
+ *
+ */
 class SimpleCalendar {
 
 	private $now = false;
 	private $daily_html = array();
 
 	/**
-	* Array of Week Day Names
-	* 
-	* @var array
-	*/
+	 * Array of Week Day Names
+	 *
+	 * @var array
+	 */
 	public $wday_names = false;
 
 	/**
-	* Constructor - Calls the setDate function
-	* 
-	* @see setDate
-	* @param null|string $date_string
-	* @return SimpleCalendar
-	*/
+	 * Constructor - Calls the setDate function
+	 *
+	 * @see setDate
+	 * @param null|string $date_string
+	 * @return SimpleCalendar
+	 */
 	function __construct( $date_string = null ) {
-		$this->setDate( $date_string );
+		$this->setDate($date_string);
 	}
 
 	/**
-	* Sets the date for the calendar
-	* 
-	* @param null|string $date_string Date string parsed by strtotime for the calendar date. If null set to current timestamp.
-	*/
+	 * Sets the date for the calendar
+	 *
+	 * @param null|string $date_string Date string parsed by strtotime for the calendar date. If null set to current timestamp.
+	 */
 	public function setDate( $date_string = null ) {
 		if( $date_string ) {
-			$this->now = getdate( strtotime( $date_string ) );
-		}else{
+			$this->now = getdate(strtotime($date_string));
+		} else {
 			$this->now = getdate();
 		}
 	}
-	
+
 	/**
 	 * Add a daily event to the calendar
 	 *
-	 * @param string $html The raw HTML to place on the calendar for this event
-	 * @param string $start_date_string Date string for when the event starts
+	 * @param string      $html The raw HTML to place on the calendar for this event
+	 * @param string      $start_date_string Date string for when the event starts
 	 * @param bool|string $end_date_string Date string for when the event ends. Defaults to start date
 	 * @return void
 	 */
 	public function addDailyHtml( $html, $start_date_string, $end_date_string = false ) {
 		static $htmlCount = 0;
-		$start_date = strtotime( $start_date_string );
-		if( $end_date_string ) { 
-			$end_date = strtotime( $end_date_string ); 
-		}else{
-			$end_date = $start_date;	
+		$start_date = strtotime($start_date_string);
+		if( $end_date_string ) {
+			$end_date = strtotime($end_date_string);
+		} else {
+			$end_date = $start_date;
 		}
-		
+
 		$working_date = $start_date;
-		do{
-			$tDate = getdate( $working_date );
+		do {
+			$tDate = getdate($working_date);
 			$working_date += 86400;
-			$this->daily_html[ $tDate['year'] ][ $tDate['mon'] ][ $tDate['mday'] ][ $htmlCount ] = $html;
-		}while( $working_date < $end_date + 1 );
-		
+			$this->daily_html[$tDate['year']][$tDate['mon']][$tDate['mday']][$htmlCount] = $html;
+		} while( $working_date < $end_date + 1 );
+
 		$htmlCount++;
-		
+
 	}
-	
+
 	/**
 	 * Clear all daily events for the calendar
 	 *
@@ -80,60 +80,69 @@ class SimpleCalendar {
 	public function clearDailyHtml() { $this->daily_html = array(); }
 
 	/**
-	* Show the Calendars current date
-	* 
-	* @param bool $show_today Whether to highlight the current day
-	* @param bool $echo Whether to echo resulting calendar
-	* @return string
-	*/
+	 * Show the Calendars current date
+	 *
+	 * @param bool $show_today Whether to highlight the current day
+	 * @param bool $echo Whether to echo resulting calendar
+	 * @return string
+	 */
 	public function show( $show_today = false, $echo = true ) {
 		if( $this->wday_names ) {
 			$wdays = $this->wday_names;
-		}else{
-			$today = ( 86400 * (date("N")) );
+		} else {
+			$today = (86400 * (date("N")));
 			for( $i = 0; $i < 7; $i++ ) {
-				$wdays[] = strftime('%a', time() - $today + ($i*86400));
+				$wdays[] = strftime('%a', time() - $today + ($i * 86400));
 			}
 		}
 
-		$wday    = date( 'N' , mktime( 0,0,1, $this->now['mon'], 1, $this->now['year'] ));
-		$no_days = cal_days_in_month( CAL_GREGORIAN, $this->now['mon'], $this->now['year'] );
+		$wday    = date('N', mktime(0, 0, 1, $this->now['mon'], 1, $this->now['year']));
+		$no_days = cal_days_in_month(CAL_GREGORIAN, $this->now['mon'], $this->now['year']);
 
 		$out = '';
 		$out .= "\n<table cellpadding=\"0\" cellspacing=\"0\" class=\"SimpleCalendar\"><thead><tr>";
-		for( $i = 0; $i < 7; $i++ ) { $out .= '<th>'. $wdays[$i]. '</th>'; }
-		
+		for( $i = 0; $i < 7; $i++ ) {
+			$out .= '<th>' . $wdays[$i] . '</th>';
+		}
+
 		$out .= "</tr></thead>\n<tbody>\n<tr>";
-		
+
 		if( $wday == 7 ) {
 			$wday = 0;
-		}else{
-			$out .= str_repeat( '<td>&nbsp;</td>', $wday );
+		} else {
+			$out .= str_repeat('<td>&nbsp;</td>', $wday);
 		}
 
 		$count = $wday + 1;
-		for($i=1;$i<=$no_days;$i++) {
+		for( $i = 1; $i <= $no_days; $i++ ) {
 			$out .= "<td>";
-			if( $show_today && $i == $this->now['mday'] ) { $out .= "<strong>$i</strong>"; } else { $out .= $i; }
-			
-			$dHtml_arr = $this->daily_html[ $this->now['year'] ][ $this->now['mon'] ][ $i ];
-			
-			if( is_array( $dHtml_arr ) ) {
+			if( $show_today && $i == $this->now['mday'] ) {
+				$out .= "<strong>$i</strong>";
+			} else {
+				$out .= $i;
+			}
+
+			$dHtml_arr = $this->daily_html[$this->now['year']][$this->now['mon']][$i];
+
+			if( is_array($dHtml_arr) ) {
 				foreach( $dHtml_arr as $eid => $dHtml ) {
 					$out .= '<div>' . $dHtml . '</div>';
 				}
 			}
-			
+
 			$out .= "</td>";
 
 			if( $count > 6 ) {
-				$out .= "</tr>\n" . ( $i != $count ? '<tr>' : '' );
+				$out .= "</tr>\n" . ($i != $count ? '<tr>' : '');
 				$count = 0;
 			}
 			$count++;
 		}
-		$out .= ( $count != 1 ? str_repeat( '<td>&nbsp;</td>', 8 - $count ) : '' ) . "</tr>\n</tbody></table>\n";		
-		if( $echo ) { echo $out; }
+		$out .= ($count != 1 ? str_repeat('<td>&nbsp;</td>', 8 - $count) : '') . "</tr>\n</tbody></table>\n";
+		if( $echo ) {
+			echo $out;
+		}
+
 		return $out;
 	}
 
