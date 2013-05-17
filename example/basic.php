2f70c6c57c54f09eab8e96e08f6bb46c06dc5bea
Jesse Donat <j.donat@capstonedigital.com>
2013-05-16 20:17:01 -0500

    Path in example fixed

--- example/basic.php
+++ example/basic.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="../lib/css/SimpleCalendar.css" />
 <?php
 error_reporting(E_ALL);
-require_once('../lib/SimpleCalendar.php');
+require_once('../lib/donatj/SimpleCalendar.php');
 
 $calendar = new donatj\SimpleCalendar();
 
