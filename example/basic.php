da46f80e3d2d101c8a9ecadd71ccfd8b911e5cd9
Jesse Donat <j.donat@capstonedigital.com>
2013-05-16 21:17:53 -0500

    Example HTML cleanup

--- example/basic.php
+++ example/basic.php
@@ -1,9 +1,14 @@
-<link rel="stylesheet" href="../lib/css/SimpleCalendar.css" />
-<?php
-error_reporting(E_ALL);
-require_once('../lib/donatj/SimpleCalendar.php');
+<html>
+<head><link rel="stylesheet" href="../lib/css/SimpleCalendar.css" /></head>
+<body>
+	<?php
+	error_reporting(E_ALL);
+	require_once('../lib/donatj/SimpleCalendar.php');
 
-$calendar = new donatj\SimpleCalendar();
+	$calendar = new donatj\SimpleCalendar();
 
-$calendar->addDailyHtml( 'Sample Event', 'today', 'tomorrow' );
-$calendar->show(true);
+	$calendar->addDailyHtml( 'Sample Event', 'today', 'tomorrow' );
+	$calendar->show(true);
+	?>
+</body>
+</html>
