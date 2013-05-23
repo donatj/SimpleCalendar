8069194839b5753c11f73687b075256cbeb6b087
Jesse Donat <j.donat@capstonedigital.com>
2013-05-23 12:28:06 -0500

    Slight example cleanup

--- example/basic.php
+++ example/basic.php
@@ -1,16 +1,16 @@
 <html>
 <head><link rel="stylesheet" href="../lib/css/SimpleCalendar.css" /></head>
 <body>
-	<?php
-	error_reporting(E_ALL);
-	require_once('../lib/donatj/SimpleCalendar.php');
+<?php
+error_reporting(E_ALL ^ E_WARNING);
+require_once('../lib/donatj/SimpleCalendar.php');
 
-	$calendar = new donatj\SimpleCalendar();
+$calendar = new donatj\SimpleCalendar();
 
-	$calendar->setStartOfWeek('Sunday');
+$calendar->setStartOfWeek('Sunday');
 
-	$calendar->addDailyHtml( 'Sample Event', 'today', 'tomorrow' );
-	$calendar->show(true);
-	?>
+$calendar->addDailyHtml( 'Sample Event', 'today', 'tomorrow' );
+$calendar->show(true);
+?>
 </body>
 </html>
